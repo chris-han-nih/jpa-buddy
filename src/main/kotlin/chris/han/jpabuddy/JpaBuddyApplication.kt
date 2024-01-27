164654dd7ceb50b5c3154df36af4a467854de261
chris.han <namilhan@gmail.com>
2024-01-27 21:29:43 +0900

    Initial commit

---
+++ src/main/kotlin/chris/han/jpabuddy/JpaBuddyApplication.kt
@@ -0,0 +1,11 @@
+package chris.han.jpabuddy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JpaBuddyApplication
+
+fun main(args: Array<String>) {
+  runApplication<JpaBuddyApplication>(*args)
+}
---
+++ src/test/kotlin/chris/han/jpabuddy/JpaBuddyApplicationTests.kt
@@ -0,0 +1,13 @@
+package chris.han.jpabuddy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JpaBuddyApplicationTests {
+  
+  @Test
+  fun contextLoads() {
+  }
+  
+}
